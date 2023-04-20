58e6206f005e8504d5cab8763b4532c4c515ee4b
Tutu Abigail <amaatutu1@gmail.com>
2023-04-20 00:16:00 +0000

    sparkle

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// let image = document.getElementById('image');
+// let images = ['1.png', '2.jpg', '3.jpg']
+// setInterval(function() {
+//     let random = Math.floor(Math.random() * 4)
+//     image.src = images[random];
+// }, 2000);
